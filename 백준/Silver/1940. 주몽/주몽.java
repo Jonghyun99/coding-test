a1199e951709410828822a4b43c9de52d14e428a
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-09-24 23:12:33 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16420 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -3,32 +3,36 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-       Scanner sc = new Scanner(System.in);
-       // 1. N 재료의 개수, M 필요한 합
-       // 2. 배열을 받는다.
-       // 3. 순환하며 합이 M이 되면 count++
-       // * 각 원소는 고유한 값을 갖고 있으므로 한번 사용되면 두번은 사용될 수 없다.
-       int N = sc.nextInt();
-       int M = sc.nextInt();
-        int answer = 0;
+        // 1. N 재료의 개수, M 필요한 합
+        // 2. 배열을 받는다.
+        // 3. 정렬한다.
+        // 4. 
+        // * 각 원소는 고유한 값을 갖고 있으므로 한번 사용되면 두번은 사용될 수 없다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System. in));
+        
+       int N = Integer.parseInt(br.readLine());
+       int M = Integer.parseInt(br.readLine());
+       int startPointer = 0;
+       int endPointer = N-1;
+       int answer = 0;
        int[] A = new int[N];
+       StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i=0; i<N; i++){
-            if(A[i]!=-1){
-                for(int j=i+1; j<N; j++) {
-                    if(A[j]!=-1){
-                    if(A[i]+A[j] == M) {
-                        answer++;
-                        A[i]=-1;
-                        A[j]=-1;
-                        break;
-                    }
-                    }
-                }
+        Arrays.sort(A);
+        while(startPointer<endPointer) {
+            if(A[startPointer]+A[endPointer]==M){
+                answer++;
+                startPointer++;
+                endPointer--;
+            } else if(A[startPointer]+A[endPointer]>M){
+                endPointer--;
+            } else {
+                startPointer++;
             }
         }
+        
         System.out.println(answer);
     }
        
