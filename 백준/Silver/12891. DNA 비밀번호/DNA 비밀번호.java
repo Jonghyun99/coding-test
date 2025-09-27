4d07e35cdf7569c9772bafcc7c411ea54eeddb14
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-09-28 01:28:37 +0900

    [Silver II] Title: DNA 비밀번호, Time: 264 ms, Memory: 23896 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,91 +1,82 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int checkArr[];
-    static int myArr[];
-    static int checkSecret;
-    public static void main(String[] args) throws IOException{
+
+    static int IDX_A = 0;
+    static int IDX_C = 1;
+    static int IDX_G = 2;
+    static int IDX_T = 3;
+    
+    static int[] currentArry = new int[4];
+    static int[] checkArray = new int[4];
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        int answer = 0;
-        char A[] = new char[S];
-        checkArr = new int[4];
-        myArr = new int[4];
 
-        checkSecret = 0;
-        A = br.readLine().toCharArray();
+        int S = Integer.parseInt(st.nextToken()); // 문자열 길이
+        int P = Integer.parseInt(st.nextToken()); // 부분문자열 길이
+
+        char[] A = new char[S];
+        A = br.readLine().toCharArray();    // 문자열 배열
+
+        int secretCount = 0;
         st = new StringTokenizer(br.readLine());
-        for (int i =0; i<4; i++) {
-            checkArr[i] = Integer.parseInt(st.nextToken());
-            if(checkArr[i] == 0)
-            checkSecret++;
+        for (int i = 0; i < 4; i++) {
+            checkArray[i] = Integer.parseInt(st.nextToken());
+            if(checkArray[i] == 0)  secretCount++;
         }
-        for (int i= 0; i<P; i++){
-            Add(A[i]);
-        }
-        if(checkSecret == 4) answer++;
 
-        for(int i = P; i< S; i++) {
-            int j=i-P;
-            Add(A[i]);
-            Remove(A[j]);
-            if (checkSecret == 4) answer++;
+        int answer = 0;
+        for (int i = 0; i < P; i++) {
+            int idx = charToIdx(A[i]);
+            if(idx != -1) {
+                currentArry[idx]++;
+                if(currentArry[idx]==checkArray[idx]) {
+                    secretCount++;
+                }
+            }
+        }
+        if(secretCount == 4) {
+            answer++;
+        }
+
+        for (int i = P; i<S; i++){
+            int j = i-P;
+            int start = charToIdx(A[j]);
+            int end = charToIdx(A[i]);
+            if(end != -1) {
+                currentArry[end]++;
+                if(currentArry[end] == checkArray[end]){
+                    secretCount++;
+                }
+            }
+            if(start != -1){
+                if(currentArry[start] == checkArray[start]){
+                    secretCount--;
+                }
+                currentArry[start]--;
+            }
+            if(secretCount==4) answer++;
         }
         System.out.println(answer);
-        br.close();
     }
 
-    private static void Remove(char c) {
+    static private int charToIdx(char c) {
         switch (c) {
             case 'A':
-            if (myArr[0] == checkArr[0]) 
-            checkSecret--;
-            myArr[0]--;
-            break;
-            case 'C' :
-            if(myArr[1] == checkArr[1])
-            checkSecret--;
-            myArr[1]--;
-            break;
-            case 'G' :
-            if(myArr[2] == checkArr[2])
-            checkSecret--;
-            myArr[2]--;
-            break;
-            case 'T' :
-            if(myArr[3] == checkArr[3])
-            checkSecret--;
-            myArr[3]--;
-            break;
+                return IDX_A;
+            case 'C':
+                return IDX_C;
+            case 'G':
+                return IDX_G;
+            case 'T':
+                return IDX_T;
+            default:
+                return -1;
         }
     }
 
-    private static void Add(char c) {
-        switch (c) {
-            case 'A':
-            myArr[0]++;
-            if (myArr[0] == checkArr[0]) 
-                checkSecret++;
-            break;
-            case 'C' :
-            myArr[1]++;
-            if(myArr[1] == checkArr[1])
-                checkSecret++;
-            break;
-            case 'G' :
-            myArr[2]++;
-            if(myArr[2] == checkArr[2])
-                checkSecret++;
-            break;
-            case 'T' :
-            myArr[3]++;
-            if(myArr[3] == checkArr[3])
-                checkSecret++;
-            break;
-        }
-    }
-       
 }
