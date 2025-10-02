e46eac0b72bad684b912697672b48a9eba439347
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-10-02 21:41:22 +0900

    [Gold IV] Title: 오큰수, Time: 936 ms, Memory: 150280 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -8,31 +8,31 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] answerArray = new int[N];
         int[] A = new int[N];
-        for(int i = 0; i<N; i++){
+        
+        for(int i=0; i<N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
+        
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        for(int i = 1; i<N; i++){
-            while(!stack.isEmpty() && A[stack.peek()] < A[i]){
+        // stack.push(0);
+        for(int i=0; i<N; i++){
+            while(!stack.isEmpty() && A[stack.peek()] < A[i]) {
                 answerArray[stack.pop()] = A[i];
             }
             stack.push(i);
         }
+
         while(!stack.isEmpty()) {
             answerArray[stack.pop()] = -1;
         }
         StringBuffer sb = new StringBuffer();
-        for(int i=0; i<N; i++) {
-            if(i==N-1){
-                sb.append(answerArray[i]);
-            } else {
-                sb.append(answerArray[i] + " ");
-            }
-            
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i=0; i<N; i++){
+            bw.write(answerArray[i] +" ");
+            // sb.append(answerArray[i]);
         }
-        System.out.println(sb);
-
+        bw.flush();
+        bw.close();
         
 
     }
