b5bf31a11446c933452012fbf9f4c48719ba7ace
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-10-06 23:11:05 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -7,17 +7,21 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] A = new int[N];
+        int[] S = new int[N];
         for(int i=0; i<N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(A);
-        int time = 0;
-        int answer = 0;
-        for(int i=0; i<A.length; i++) {
-            time += A[i];
-            answer +=time;
+        
+        S[0] = A[0];
+        for(int i=1; i<N; i++) {
+            S[i] = S[i-1] + A[i];
         }
-        System.out.println(answer);
+        int sum =0;
+        for(int i=0; i<N; i++) {
+            sum+=S[i];
+        }
+        System.out.println(sum);
     }
 
 }
