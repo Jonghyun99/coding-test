a9b7b1825f03656acce6e7359eb5f06598695ddb
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-10-14 23:22:16 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 984 ms, Memory: 114764 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,21 +1,58 @@
-
-import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class Main{
+
+    public static int[] A,tmp;
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        for(int i=0; i<N; i++){
+        A = new int[N+1];
+        tmp = new int[N+1];
+        for(int i =1; i<=N; i++){
             A[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(A);
-        
-        for(int i=0; i<A.length; i++) {
-            System.out.println(A[i]);
+        merget_sort(1, N);
+        for(int i=1; i<=N; i++){
+            bw.write(A[i] + "\n");
         }
+        bw.flush();
+        bw.close(); 
     }
 
-}
+    private static void merget_sort(int s, int e) {
+        if(e-s<1) return;
+        int m = s+(e-s) / 2;
 
+        merget_sort(s, m);
+        merget_sort(m+1, e);
+        for(int i=s; i<=e; i++){
+            tmp[i] = A[i];
+        }
+        int k=s;
+        int index1 = s;
+        int index2 = m+1;
+        while(index1<=m && index2 <= e){
+            if(tmp[index1] > tmp[index2]) {
+                A[k] = tmp[index2];
+                k++;
+                index2++;
+            } else {
+                A[k] = tmp[index1];
+                k++;
+                index1++;
+            }
+        }
+        while(index1 <= m){
+            A[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+        while(index2 <= e){
+            A[k] = tmp[index2];
+            k++;
+            index2++;
+        }
+    }
+}
