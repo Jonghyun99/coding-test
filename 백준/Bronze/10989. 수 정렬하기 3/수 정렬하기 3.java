5e39cd798cc602d4b3584aedbf498df505949ecb
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-10-27 10:43:04 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2208 ms, Memory: 340788 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,39 +1,37 @@
+
 import java.io.*;
 
 public class Main{
-    public static int[] A;
-
-
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
-        A = new int[N];
+        int[] A = new int[N];
         int max = Integer.MIN_VALUE;
         for(int i=0; i<N; i++){
             A[i] = Integer.parseInt(br.readLine());
-            if(max<A[i]) max = A[i];
+            if(A[i] > max){
+                max = A[i];
+            }
         }
-        br.close();
+
         radixSort(A, max);
-        for(int i=0; i<A.length; i++){
+        for(int i=0;i<N; i++){
             bw.write(A[i] + "\n");
         }
         bw.flush();
         bw.close();
-        
-
     }
 
-    public static void radixSort(int[] A, int maxNumber){
+    public static void radixSort(int[] A, int max){
         int exp = 1;
         int[] output = new int[A.length];
-
-
-        while(maxNumber / exp > 0) {
+        while(max/exp > 0) {
             int[] count = new int[10];
+
             for(int i=0; i<A.length; i++){
-                count[(A[i]/exp) % 10]++;
+                count[(A[i]/exp)%10]++;
             }
 
             for(int i=1; i<10; i++){
@@ -41,17 +39,19 @@ public class Main{
             }
 
             for(int i=A.length-1; i>=0; i--){
-                int digit = (A[i] / exp) % 10;
-                int position = count[digit] - 1;
-                output[position] = A[i];
+                // count[i]의 누적합 값을  out
+                int digit = (A[i]/exp)%10;
+                output[count[digit]-1] = A[i];
                 count[digit]--;
-                
             }
+
             for(int i=0; i<A.length; i++){
                 A[i] = output[i];
             }
             exp*=10;
         }
     }
+
+    
        
 }
