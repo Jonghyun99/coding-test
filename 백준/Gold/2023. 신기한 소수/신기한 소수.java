e7f402d4748422755ce5f2fa10ee557ea624a417
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-10-31 09:09:42 +0900

    [Gold V] Title: 신기한 소수, Time: 108 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,65 +1,46 @@
 
+
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 
 public class Main{
-    
+
     static int N;
-    static Deque<State> stack;
-
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        stack = new ArrayDeque<>();
-        stack.push(new State(7,1));
-        stack.push(new State(5,1));
-        stack.push(new State(3,1));
-        stack.push(new State(2,1));
-        DFS(stack);
-    }
     
-    static void DFS(Deque<State> stack) {
-        while(!stack.isEmpty()){
-            State current = stack.pop();
-            int number = current.number;
-            int digit = current.digit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
 
-            if(digit == N){
-                if(isPrime(number)) {
-                    System.out.println(number);
-                    continue;
-                }
-            }
-            
-            for(int i=9; i>=1; i-=2){
-                if(isPrime(number*10+i)){
-                    // DFS(number*10+i,digit+1);
-                    stack.push(new State(number*10+i,digit+1));
-                }
+        DFS(2,1);
+        DFS(3,1);
+        DFS(5,1);
+        DFS(7,1);
+    }
+
+    static void DFS(int number, int exp){
+        if(exp==N) System.out.println(number);
+
+        for(int i=1; i<=9; i+=2){
+            if(isPrime(number*10+i)){
+                DFS(number*10+i,exp+1);
+                if(exp==N) return;
             }
         }
     }
 
     static boolean isPrime(int number){
-        if(number < 2) return false;
+        if(number<2) return false;
 
-        for(int i=2; i*i <= number; i++){
+        // 범위 = 맞는지 확인
+        for(int i=2;i*i<=number;i++){
             if(number%i==0) return false;
         }
-
         return true;
-    }   
-}
-class State{
-    int number;
-    int digit;
 
-    public State(int number, int digit){
-        this.number = number;
-        this.digit = digit;
     }
+
+    
 }
