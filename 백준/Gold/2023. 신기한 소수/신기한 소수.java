a225636b3c3d5bfdf5450cb2d1674310d0681618
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2025-11-08 12:21:44 +0900

    [Gold V] Title: 신기한 소수, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,9 +1,7 @@
 
-
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
-
-
 
 public class Main{
 
@@ -12,35 +10,32 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-
-        DFS(2,1);
-        DFS(3,1);
-        DFS(5,1);
-        DFS(7,1);
+        N=Integer.parseInt(st.nextToken());
+        dfs(2,1);
+        dfs(3,1);
+        dfs(5,1);
+        dfs(7,1);
     }
 
-    static void DFS(int number, int exp){
-        if(exp==N) System.out.println(number);
+    static void dfs(int num, int length){
+        if(length == N) {
+            if(isPrime(num)){
+                System.out.println(num);
+            }
+            return;
+        }
 
         for(int i=1; i<=9; i+=2){
-            if(isPrime(number*10+i)){
-                DFS(number*10+i,exp+1);
-                if(exp==N) return;
+            if(isPrime(num*10+i)){
+                dfs(num*10+i,length+1);
             }
         }
     }
 
-    static boolean isPrime(int number){
-        if(number<2) return false;
-
-        // 범위 = 맞는지 확인
-        for(int i=2;i*i<=number;i++){
-            if(number%i==0) return false;
+    static boolean isPrime(int num){
+        for(int i=2; i*i<=num; i++){
+            if(num%i==0) return false;
         }
         return true;
-
     }
-
-    
 }
