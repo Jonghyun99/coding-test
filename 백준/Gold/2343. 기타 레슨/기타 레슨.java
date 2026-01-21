41a24ce41d71496cecd5c62a175a766d9eb46363
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-01-21 13:15:49 +0900

    [Gold V] Title: 기타 레슨, Time: 256 ms, Memory: 23864 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.java
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -2,34 +2,41 @@
     import java.io.*;
 
     class Main{
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int N = sc.nextInt();
-            int M = sc.nextInt();
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            
             int[] A = new int[N];
+            st = new StringTokenizer(br.readLine());
+
             int start = 0;
-            int end =0;
-            for (int i=0; i<N; i++){
-                A[i] = sc.nextInt();
-                if(start <A[i]) start = A[i];
-                end = end + A[i];
+            int end = 0;
+
+            for(int i=0; i<N; i++) {
+                A[i] = Integer.parseInt(st.nextToken());
+                if(A[i]> start) start = A[i];
+                end += A[i];
             }
-            while(start <= end) {
-                int mid = (start + end) / 2;
+
+            while(start<=end){
+                int middle = (start + end) / 2;
                 int sum = 0;
                 int count = 0;
-                for (int i=0; i<N; i++) {
-                    if(sum + A[i] > mid) {
+
+                for(int i=0; i<N; i++){
+                    if(sum+A[i] > middle) {
                         count++;
                         sum = 0;
                     }
-                    sum = sum + A[i];
+                    sum+=A[i];
                 }
-                if (sum != 0) count++;
+                if(sum!=0) count++;
                 if(count>M) {
-                    start = mid + 1;
+                    start = middle+1;
                 } else {
-                    end = mid - 1;
+                    end = middle - 1;
                 }
             }
             System.out.println(start);
