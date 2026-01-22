74586c1302b7ec7dd35307a965c752ac8bba0874
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-01-22 11:46:26 +0900

    [Silver IV] Title: 주몽, Time: 160 ms, Memory: 16420 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,39 +1,46 @@
-import java.util.*;
-import java.io.*;
+    import java.util.*;
+    import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
-        // 1. N 재료의 개수, M 필요한 합
-        // 2. 배열을 받는다.
-        // 3. 정렬한다.
-        // 4. 
-        // * 각 원소는 고유한 값을 갖고 있으므로 한번 사용되면 두번은 사용될 수 없다.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System. in));
-        
-       int N = Integer.parseInt(br.readLine());
-       int M = Integer.parseInt(br.readLine());
-       int startPointer = 0;
-       int endPointer = N-1;
-       int answer = 0;
-       int[] A = new int[N];
-       StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(A);
-        while(startPointer<endPointer) {
-            if(A[startPointer]+A[endPointer]==M){
-                answer++;
-                startPointer++;
-                endPointer--;
-            } else if(A[startPointer]+A[endPointer]>M){
-                endPointer--;
-            } else {
-                startPointer++;
+    class Main{
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int N = Integer.parseInt(st.nextToken());
+            
+            st = new StringTokenizer(br.readLine());
+
+            int M = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+
+            int[] A = new int[N];
+            for(int i=0; i<N; i++){
+                A[i] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(A);
+
+            int start = 0;
+            int end = A.length-1;
+            int cnt = 0;
+            int sum = A[start] + A[end];
+
+            while(start<end){
+                if(sum == M) {
+                    end--;
+                    start++;
+                    sum = A[start] + A[end];
+                    cnt++;
+                } else if (sum > M) {
+                    sum-=A[end];
+                    end--;
+                    sum+=A[end];
+                } else {
+                    sum-=A[start];
+                    start++;
+                    sum+=A[start];
+                }
+            }
+            System.out.println(cnt);
         }
-        
-        System.out.println(answer);
     }
-       
-}
