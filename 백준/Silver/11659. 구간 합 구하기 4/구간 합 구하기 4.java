cd0e3bd0c94e2c51dff60ebb96f8a347995901fa
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-01-22 12:38:15 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 948 ms, Memory: 58452 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,22 +1,27 @@
-import java.io.*;
-import java.util.*;
-public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int numCount = Integer.parseInt(st.nextToken());
-        int queryCount = Integer.parseInt(st.nextToken());
-        int[] sumArray = new int[numCount+1];
-        st = new StringTokenizer(br.readLine());
-        sumArray[0]=0;
-        for(int i=1; i<=numCount;i++){
-            sumArray[i] = sumArray[i-1] + Integer.parseInt(st.nextToken());
-        }
-        for(int idx=0; idx<queryCount; idx++){
+    import java.util.*;
+    import java.io.*;
+
+    class Main{
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            int[] S = new int[N+1];
+            
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            System.out.println(sumArray[j] - sumArray[i-1]);
+            for(int i=1; i<=N; i++){
+                S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+            }
+
+            for(int i=0; i<M; i++){
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+
+                System.out.println(S[end] - S[start-1]);
+            }
         }
     }
-}
