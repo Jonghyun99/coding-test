5f7bf0130244caad8f9d1f966f96a91cc63286ed
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-01-22 13:50:57 +0900

    [Gold I] Title: K번째 수, Time: 168 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -6,22 +6,21 @@
             Scanner sc = new Scanner(System.in);
             int N = sc.nextInt();
             int K = sc.nextInt();
-            long start = 1, end = K;
+
+            long start = 1;
+            long end = K;
             long ans = 0;
-
-            while(start<=end) {
-                long middle = (start + end) / 2;
+            while(start <= end) {
                 long cnt = 0;
-
-                // 중앙 값보다 작은 수 몇개인지 계산하기 (1. 왜 N이랑 비교하는지?)
+                long middle = (start+end)/2;
                 for(int i=1; i<=N; i++){
-                    cnt += Math.min(middle / i,N);  // 핵심로직
+                    cnt+=Math.min(middle/i,N);
                 }
-                if(cnt<K) {
-                    start = middle + 1;
+                if(cnt<K){
+                    start = middle+1;
                 } else {
+                    end = middle-1;
                     ans = middle;
-                    end = middle - 1;
                 }
             }
             System.out.println(ans);
