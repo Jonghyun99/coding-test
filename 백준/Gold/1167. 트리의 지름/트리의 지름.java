83ff5279c9ca1ccb3b31ca95e20ded92d0b2cfc0
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-01-24 12:45:54 +0900

    [Gold II] Title: 트리의 지름, Time: 800 ms, Memory: 103356 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,70 +1,80 @@
     import java.util.*;
     import java.io.*;
 
-class Main{
+    class Main{
 
-    static class Edge{
-        int to;
-        int weight;
+        static ArrayList<Node>[] nodes;
+        static boolean[] visited;
+        static int maxDistance = 0;
+        static int maxNode = 0;
 
-        public Edge(int to, int weight){
-            this.to = to;
-            this.weight = weight;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int V = Integer.parseInt(st.nextToken());
+
+            nodes = new ArrayList[V+1];
+            for(int i=1; i<=V; i++){
+                nodes[i] = new ArrayList<>();
+            }
+            for(int i=1; i<=V; i++){
+                st = new StringTokenizer(br.readLine());
+                
+                int nodeNum = Integer.parseInt(st.nextToken());
+
+                while(true) {
+                    int nowVertex = Integer.parseInt(st.nextToken());
+                    if(nowVertex == -1) break;
+                    int nowLength = Integer.parseInt(st.nextToken());
+                    nodes[nodeNum].add(new Node(nowVertex, nowLength));
+                }
+            }
+
+            // 트리의 지름 구하는 로직 스타트 DFS를 이용하여 임의의 점에서 가장 먼 길이의 노드를 구하고
+            // 그 노드에서 출발하여 가장 먼 노드의 길이 값을 구하라
+            // + dfs, bfs 사용예 복습하기
+            visited = new boolean[V+1];
+            DFS(1,0);
+            visited = new boolean[V+1];
+            DFS(maxNode, 0);
+            System.out.println(maxDistance);
+
+
         }
-    }
 
-    static ArrayList<Edge>[] list;
-    static boolean[] visited;
-    static int maxDist = 0;
-    static int farthestNode = 0;
+        static void DFS(int currentNode, int currentLength) {
+            
+            visited[currentNode] = true;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken());
+            if(maxDistance<currentLength) {
+                maxDistance = currentLength;
+                maxNode = currentNode;
+            }
 
-        list = new ArrayList[V + 1];
-        for(int i=1; i<=V; i++){
-            list[i] = new ArrayList<>();
-        }
+            for(Node next:nodes[currentNode]){
+                int vertex = next.vertex;
+                int length = next.length;
+                if(!visited[vertex]){
+                    DFS(vertex,currentLength + length);
+                }
 
-        for(int i=0; i<V; i++){
-            st = new StringTokenizer(br.readLine());
-            int node = Integer.parseInt(st.nextToken());
-
-            while(true){
-                int linkedNode = Integer.parseInt(st.nextToken());
-                if(linkedNode==-1) break;
-
-                int weight = Integer.parseInt(st.nextToken());
-                list[node].add(new Edge(linkedNode, weight));
             }
         }
 
-        // 임의 노드에서 가장 먼 노드 찾기
-        visited = new boolean[V+1];
-        dfs(1, 0);        
 
-        visited = new boolean[V+1];
-        maxDist = 0;
-        dfs(farthestNode, 0);
 
-        System.out.println(maxDist);
-    }
 
-    public static void dfs(int node, int dist){
-        if(dist > maxDist) {
-            maxDist = dist;
-            farthestNode = node;
-        }
 
-        visited[node] = true;
+        static class Node {
+            int vertex;
+            int length;
 
-        for(Edge edge:list[node]){
-            if(!visited[edge.to]){
-                dfs(edge.to, dist+edge.weight);
+            Node(int vertex, int length) {
+                this.vertex = vertex;
+                this.length = length;
             }
         }
+
+
     }
-    
-}
