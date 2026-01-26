fdf1833ea92bd3016a323879cc85382c466f5d90
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-01-26 12:42:04 +0900

    [Gold IV] Title: 수 묶기, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -7,46 +7,49 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        PriorityQueue<Integer> plusPq = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minusPq = new PriorityQueue<>();
+
+        PriorityQueue<Integer> plusQueue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minusQueue = new PriorityQueue<>();
+
         int one = 0;
         int zero = 0;
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            int data = Integer.parseInt(st.nextToken());
-            if(data > 1) {
-                plusPq.add(data);
-            } else if (data == 1) {
+            int n = Integer.parseInt(st.nextToken());
+            if(n>1) {
+                plusQueue.add(n);
+            } else if(n==1) {
                 one++;
-            } else if(data==0){
+            } else if (n==0) {
                 zero++;
             } else {
-                minusPq.add(data);
+                minusQueue.add(n);
             }
         }
+
         int sum = 0;
-        while(plusPq.size()>1) {
-            int first = plusPq.remove();
-            int second = plusPq.remove();
-            sum += first * second;
+        while(plusQueue.size()>1) {
+            int num1 = plusQueue.poll();
+            int num2 = plusQueue.poll();
+
+            sum+= num1*num2;
+        }
+        if(!plusQueue.isEmpty()) {
+            sum += plusQueue.poll();
         }
 
-        if(!plusPq.isEmpty()) {
-            sum += plusPq.remove();
+        while(minusQueue.size()>1) {
+            int num1 = minusQueue.poll();
+            int num2 = minusQueue.poll();
+            sum += num1*num2;
         }
 
-        while(minusPq.size()>1) {
-            int first = minusPq.remove();
-            int second = minusPq.remove();
-            sum += first * second;
-        }
-        if(!minusPq.isEmpty()) {
-            if(zero == 0){
-                sum += minusPq.remove();
-            }
+        if(!minusQueue.isEmpty()) {
+            if(zero==0) sum += minusQueue.poll();
         }
 
         sum += one;
+
         System.out.println(sum);
     }
 
