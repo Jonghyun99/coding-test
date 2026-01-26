603ff46435427bad29b63783380b7f87fc2c8cae
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-01-26 12:32:32 +0900

    [Gold IV] Title: 수 묶기, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> plusPq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minusPq = new PriorityQueue<>();
+        int one = 0;
+        int zero = 0;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int data = Integer.parseInt(st.nextToken());
+            if(data > 1) {
+                plusPq.add(data);
+            } else if (data == 1) {
+                one++;
+            } else if(data==0){
+                zero++;
+            } else {
+                minusPq.add(data);
+            }
+        }
+        int sum = 0;
+        while(plusPq.size()>1) {
+            int first = plusPq.remove();
+            int second = plusPq.remove();
+            sum += first * second;
+        }
+
+        if(!plusPq.isEmpty()) {
+            sum += plusPq.remove();
+        }
+
+        while(minusPq.size()>1) {
+            int first = minusPq.remove();
+            int second = minusPq.remove();
+            sum += first * second;
+        }
+        if(!minusPq.isEmpty()) {
+            if(zero == 0){
+                sum += minusPq.remove();
+            }
+        }
+
+        sum += one;
+        System.out.println(sum);
+    }
+
+}
