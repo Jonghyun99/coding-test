6b6261fe6d89a927fe0afcbb27c51fa6ac27b3d8
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-01 13:11:41 +0900

    [Gold IV] Title: 강의실 배정, Time: 560 ms, Memory: 67956 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -5,36 +5,38 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        
+        int N = Integer.parseInt(br.readLine());
+
         int[][] lectures = new int[N][2];
 
+        StringTokenizer st;
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             lectures[i][0] = Integer.parseInt(st.nextToken());
             lectures[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        // 강의실이 1개일 때와는 다르게 시작시간 오름차순으로 정렬한다. 왜? 그래야 겹치는 강의들을 판별할 수 있으니까
-        Arrays.sort(lectures, (lectrue1, lectrue2) -> {
-            if(lectrue1[0] == lectrue2[0]) 
-                return Integer.compare(lectrue1[1], lectrue2[1]);
-            return Integer.compare(lectrue1[0], lectrue2[0]);
+        Arrays.sort(lectures,(o1, o2) -> {
+            // 시작시간 정렬하나 끝나는 시간은 정말 중요하지 않은지 확인하기
+            // 시작 시간이 같은 경우, 끝나는 시간이 더 짧은 것을 넣어야 하기 때문에 상관없지않음.. 
+            // ex 1~3 , 1~5, 3~5 가 있을경우 정렬하지 않으면 2개, 정렬하면 1개를 넣을 수 있음
+            if(o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
+            return Integer.compare(o1[0], o2[0]);
         });
 
-        // 우선순위 큐 선언, (종료시간을 저장, 자동으로 오름차순 정렬됨)
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(lectures[0][1]); //첫번째 수업 큐에 넣기
+        PriorityQueue<Integer> endTimeQueue = new PriorityQueue<>();
+        endTimeQueue.add(lectures[0][1]);
         
-        //2번째 큐부터 순회
-        for(int i=1; i < N; i++) {
-            // 현재 가장 빨리 끝나는 강의실의 종료시간 <= 다음 수업의 시작 시간
-            if(pq.peek() <= lectures[i][0]) {
-                pq.poll();  // 강의실을 이어 쓸 수 있을 경우에는 하나 제거
+        
+        for(int i=1; i<N; i++) {
+            if(endTimeQueue.peek() <= lectures[i][0]){
+                endTimeQueue.poll();
             }
-            pq.add(lectures[i][1]);
+
+            endTimeQueue.add(lectures[i][1]);
         }
-        System.out.println(pq.size());
+
+        System.out.println(endTimeQueue.size());
+        
     }
 }
