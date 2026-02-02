21f8fb1aaf6212d20b89819d1f48dc30ddcb171d
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-02 13:50:26 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,20 +3,25 @@ import java.io.*;
 
 class Main {
 
-    // 그리디 핵심 로직: 뺄셈의 뒤는 플..
-    // 100-40+50+74-30+29-45+43+11 ... -222
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        String[] minusStrArray = str.split("-");
-        int sum = 0;
-        for(int i=0; i<minusStrArray.length; i++){
-            String[] minusStr = minusStrArray[i].split("[+]");
-            for(int j=0; j<minusStr.length; j++){
-                if(i==0){
-                    sum += Integer.parseInt(minusStr[j]);
+        StringTokenizer st = new StringTokenizer(br.readLine(),"-+",true);
+
+        int sum = Integer.parseInt(st.nextToken());
+        boolean isMinusSeen = false;
+
+        while(st.hasMoreTokens()) {
+            String token = st.nextToken();
+
+            if(token.equals("-")){
+                isMinusSeen = true;
+            } else if(token.equals("+")){
+
+            } else {
+                if(isMinusSeen) {
+                    sum-=Integer.parseInt(token);
                 } else {
-                    sum -= Integer.parseInt(minusStr[j]);
+                    sum+=Integer.parseInt(token);
                 }
             }
         }
