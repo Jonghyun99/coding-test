9213db9a4c7553a81e7c93e23af00a76db537d71
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-03 12:26:19 +0900

    [Silver III] Title: 소수 구하기, Time: 272 ms, Memory: 23920 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -7,26 +7,31 @@ class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        // 에라토노스의체 
-        // n부터 m까지 본인을 제외한 배수를 제거하며 소수를 판별한다.
-        boolean[] A = new boolean[M+1];
-        Arrays.fill(A, true);
+        boolean[] isPrime = new boolean[M+1];
+        Arrays.fill(isPrime, true);
 
-        A[0] = false;
-        A[1] = false;
-        
-        //소수 판별
-        for(int i=2; i<=Math.sqrt(M); i++) {
-            if(!A[i]) continue;
-            
-            // i의 배수 반복문 
-            for(int j=i+i; j<=M; j=j+i) {
-                A[j] = false;
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        // 약수대칭성, i의 제곱이 M보다 커지면 더이상 i로 떨어지는 M의 약수는 없음
+        for(int i=2; i*i<=M; i++){
+
+            // i가 소수라면 i의 배수 지우기
+            if(isPrime[i]) {
+                // j= i*2가 아닌 i*i부터 시작해도 됨
+                // 왜나하면  i의 2,3,4.. i-1들은 i까지의 반복문에서 이미 지워짐 
+                for(int j=i*i; j<=M; j+=i) {
+                    isPrime[j] = false;
+                }
             }
         }
 
-        for(int i=N; i<=M; i++) {
-            if(A[i]) System.out.println(i);
+        StringBuffer sb = new StringBuffer();
+        for(int i=N; i<=M; i++){
+            if(isPrime[i]){
+                sb.append(i).append("\n");
+            }
         }
+        System.out.println(sb.toString());
     }
 }
