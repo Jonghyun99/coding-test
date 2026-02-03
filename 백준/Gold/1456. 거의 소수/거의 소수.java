79b14c92951ebbf24a06cec476a902356ea95b86
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-03 13:49:53 +0900

    [Gold V] Title: 거의 소수, Time: 296 ms, Memory: 27664 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+
+        int limit = (int)Math.sqrt(B);
+
+        boolean[] isPrime = new boolean[limit+1];
+        Arrays.fill(isPrime,true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i=2; i<=Math.sqrt(limit); i++) {
+            if(isPrime[i]) {
+                for(int j=i*i; j<=limit; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        long cnt = 0;
+        for(int i=2; i<=limit; i++){
+            if(isPrime[i]) {
+                long temp = i;
+                while(temp<=B/i) {
+                    temp *= i;
+                    if(temp>=A) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
