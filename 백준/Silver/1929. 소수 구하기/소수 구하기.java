837e7fc73b5d7c3ddc5b556f15dd734bc27e83e7
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-03 12:35:02 +0900

    [Silver III] Title: 소수 구하기, Time: 260 ms, Memory: 23196 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -8,30 +8,26 @@ class Main {
         int M = sc.nextInt();
 
         boolean[] isPrime = new boolean[M+1];
-        Arrays.fill(isPrime, true);
+        Arrays.fill(isPrime,true);
 
         isPrime[0] = false;
         isPrime[1] = false;
 
-        // 약수대칭성, i의 제곱이 M보다 커지면 더이상 i로 떨어지는 M의 약수는 없음
         for(int i=2; i*i<=M; i++){
-
-            // i가 소수라면 i의 배수 지우기
             if(isPrime[i]) {
-                // j= i*2가 아닌 i*i부터 시작해도 됨
-                // 왜나하면  i의 2,3,4.. i-1들은 i까지의 반복문에서 이미 지워짐 
-                for(int j=i*i; j<=M; j+=i) {
+                for(int j=i*i; j<=M; j=j+i) {
                     isPrime[j] = false;
                 }
             }
         }
 
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for(int i=N; i<=M; i++){
-            if(isPrime[i]){
-                sb.append(i).append("\n");
+            if(isPrime[i]) {
+                sb.append(i);
+                sb.append('\n');
             }
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
