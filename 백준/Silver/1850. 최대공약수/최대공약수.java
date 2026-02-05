dfb0d1845f907e7ea781a9105ae526dd21bbafd9
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-05 09:47:17 +0900

    [Silver I] Title: 최대공약수, Time: 364 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        // long result = gcd1(a,b);
+        long result = gcd(A,B);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i=0; i<result; i++){
+            bw.write("1");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static long gcd1(long a,long b) {
+        if(b==0) {
+            return a;
+        } else {
+            return gcd1(b, a%0);
+        }
+    }
+
+    static long gcd(long a,long b) {
+        while(b>0){
+            long temp = a%b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
