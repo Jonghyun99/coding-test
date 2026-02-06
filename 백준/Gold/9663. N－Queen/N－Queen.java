7d2ca8b871b02b4a9858044b8300ddfc08f4bb43
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-06 13:07:24 +0900

    [Gold IV] Title: N-Queen, Time: 4832 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,31 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
-    static int N;
-    // static 
-    static int cnt = 0;
     static int[] A;
+    static int N;
+    static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         A = new int[N];
-        DFS(0);
+        backtracking(0);
         System.out.println(cnt);
     }
 
-    static void DFS(int row){
-        if(row == N) {
+    static void backtracking(int row) {
+        if(row==N) {
             cnt++;
             return;
         }
-
         for(int i=0; i<N; i++){
             A[row] = i;
             if(check(row)){
-                DFS(row+1);
+                backtracking(row+1);
             }
         }
     }
@@ -33,23 +41,12 @@ public class Main{
     static boolean check(int row){
         for(int i=0; i<row; i++){
             if(A[i] == A[row]) return false;
-            if(Math.abs(i-row) == Math.abs(A[row] - A[i])) return false;
+            
+            // 여기가 중요, 인덱스의 차이와 배치된 칸의 차이가 같을 경우 대각선에 존재함
+            if(Math.abs(row-i) == Math.abs(A[i] - A[row])) return false; // 대각선에 있는지 체크
         }
+
         return true;
     }
 
-    
-
-    
-
-    
-
-
-
-
-
-    
-
- 
-    
 }
