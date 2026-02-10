2a3544ad8257c3126c40ff1c180a7ba774338a93
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-10 11:48:44 +0900

    [Gold V] Title: 집합의 표현, Time: 896 ms, Memory: 51744 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,46 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static int[] parent;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         parent = new int[N+1];
-        for(int i = 1; i<=N; i++){
+        
+        for(int i=0; i<=N; i++) {
             parent[i] = i;
         }
 
         for(int i=0; i<M; i++) {
-            int q = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            if(q==0){
+            st = new StringTokenizer(br.readLine());
+            int q = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(q==0) {
                 union(a,b);
             } else {
-                if(find(a)==find(b)) {
+                if(find(a)==find(b))
                     System.out.println("YES");
-                } else {
+                else 
                     System.out.println("NO");
-                }
             }
         }
-    }     
+    }
 
-    static int find(int a){
+    static int find(int a) {
         if(parent[a] == a) return a;
         return parent[a] = find(parent[a]);
     }
 
-    static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if(a!=b){
-            parent[b] = a;
+    static void union(int a,int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA != rootB){
+            parent[rootB] = rootA;
+            // 1 2 3 4 5 에서 union (1 3) 할 시 1이 3의 부모가 됨
+            // parent[3] = 1이 나와야함
+            // 고로 union(a,b)에서 parent[b] 할시 a가 나와야 하므로
+            // parent[b] = a 순서가 맞지?
         }
     }
 
