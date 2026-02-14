e3949235b9fca41d51ea9e122588f725748f893c
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-14 14:05:04 +0900

    [Gold V] Title: 최소비용 구하기, Time: 400 ms, Memory: 51048 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -10,27 +10,12 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
-
-// 도시의 개수 1<=N<=1000, 버스의 개수 1<=M<=100000
-// 3번째 줄: 버스의 정보, startCity, endCity, price
-// 마지막 줄: 임의 출발점과 도착점번호
-// 마지막 줄의 노드 간 최소 비용을 출력해라
-// 시간제한 0.5초, 다익스트라 사용, 예상 시간복잡도 O(V+E)??
-
-// visited, adjGraph, 각 도시별 최소금액 담을 금액, visited 대신 money 변수로 사용
-// 내가 생각한 풀이법: 출발점에서 모든 노드를 통한 도착지점을 향한 가장 싼 경로를 탐색한다.
-// 현재 가장 저렴한 루트를 찾기 위해 Priroity 사용, (굳이 Prirotiy Queue안써도 DFS나 BFS만으로도 될 것 같은데 맞나?)
-// 도중 현재까지의 가장 싼 경로보다 비싸지면, 해당 탐색은 취소하고 이어간다.
-// 현재까지의 가장 싼 경로는 배열이 아닌 int 변수에 담아서 해보자
-// 흠.. visited 배열이 필요한가? money 비교로 커버 되려나??
-
 public class Main {
-    static int resultMoney = Integer.MAX_VALUE;
-    static ArrayList<BusRoute>[] busGraph;
-    static int cityCnt, busCnt, finalStartCity, finalEndCity;
+
+    // 도시 별 소요 최소금액
     static int[] minMoney;
-    final static int INF = Integer.MAX_VALUE;
+    static ArrayList<BusRoute>[] busGraph;
+    static int cityCnt, busCnt, firstCity, lastCity;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,33 +23,35 @@ public class Main {
         busCnt = Integer.parseInt(br.readLine());
         busGraph = new ArrayList[cityCnt+1];
         minMoney = new int[cityCnt+1];
-        
-        for(int i=0; i<=cityCnt; i++){
-            busGraph[i] = new ArrayList<>();
-        }
-        Arrays.fill(minMoney, INF);
-        StringTokenizer st;
-        for(int i=0; i<busCnt; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int price = Integer.parseInt(st.nextToken());
 
-            busGraph[start].add(new BusRoute(end, price));
+        for(int i=0; i<=cityCnt; i++) {
+            busGraph[i] = new ArrayList<>();
+            minMoney[i] = Integer.MAX_VALUE;
+        }
+
+        StringTokenizer st;
+        for(int i=0; i<busCnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            busGraph[s].add(new BusRoute(e, p));
         }
 
         st = new StringTokenizer(br.readLine());
-        finalStartCity = Integer.parseInt(st.nextToken());
-        finalEndCity = Integer.parseInt(st.nextToken());
+        firstCity = Integer.parseInt(st.nextToken());
+        lastCity = Integer.parseInt(st.nextToken());
 
-        dijkstra(finalStartCity, finalEndCity);
-        System.out.println(minMoney[finalEndCity]);
+        dijkStra(firstCity, lastCity);
+
+        System.out.println(minMoney[lastCity]);
     }
 
-    static void dijkstra(int s, int e) {
+    static void dijkStra(int firstCity, int lastCity) {
         PriorityQueue<BusRoute> q = new PriorityQueue<>();
-        q.add(new BusRoute(s, 0));
-        minMoney[s] = 0;
+        q.add(new BusRoute(firstCity, 0));
+        minMoney[firstCity] = 0;
         while(!q.isEmpty()) {
             BusRoute now = q.poll();
             int nowCity = now.city;
@@ -72,20 +59,20 @@ public class Main {
 
             if(minMoney[nowCity] < nowPrice) continue;
 
-            for(BusRoute next:busGraph[nowCity]){
+            for(BusRoute next:busGraph[nowCity]) {
                 int nextCity = next.city;
-                int nextPrice = nowPrice + next.price; 
+                int nextPrice = next.price + nowPrice;
 
-                if(minMoney[nextCity] > nextPrice){
-                    minMoney[nextCity] = nextPrice;
+                if(minMoney[nextCity]>nextPrice) {
                     q.add(new BusRoute(nextCity, nextPrice));
+                    minMoney[nextCity] = nextPrice;
                 }
             }
         }
     }
 }
 
-class BusRoute implements Comparable<BusRoute> {
+class BusRoute implements Comparable<BusRoute>{
     int city;
     int price;
 
