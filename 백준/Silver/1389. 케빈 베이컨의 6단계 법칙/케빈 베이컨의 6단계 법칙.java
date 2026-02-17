7e910a67365b11464b35d63fe3313284475768ab
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-17 12:47:12 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 100 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -3,36 +3,35 @@ import java.util.*;
 
 public class Main{
 
-    static int N,M;
-    static int[][] dist;
     static int INF = 987654321;
-
+    static int[][] dist;
+    static int N,M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         dist = new int[N+1][N+1];
-
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=N; j++) {
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
                 if(i==j) dist[i][j] = 0;
                 else dist[i][j] = INF;
             }
         }
 
+        // 왜 bfs는 메소드를 분리하는데 플로이드 워셜은 분리안할까?
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            dist[a][b] = dist[b][a] = 1;
+            dist[a][b] = 1;
+            dist[b][a] = 1;
         }
 
-        for(int k=1; k<=N; k++){
-            for(int i=1; i<=N; i++){
+        for(int k=1; k<=N; k++) {
+            for(int i=1; i<=N; i++) {
                 for(int j=1; j<=N; j++){
                     dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
@@ -40,11 +39,11 @@ public class Main{
         }
 
         int minSum = INF;
-        int resultPerson = 1;
+        int resultPerson = -1;
 
         for(int i=1; i<=N; i++){
             int sum = 0;
-            for(int j=1; j<=N; j++){
+            for(int j=1; j<=N; j++) {
                 sum += dist[i][j];
             }
             if(minSum>sum) {
