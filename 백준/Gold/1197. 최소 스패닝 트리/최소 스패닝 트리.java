8a2816f83bcb0b977adcfa76c6c0c524674ef405
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-19 11:30:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 564 ms, Memory: 49444 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -12,81 +12,85 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
     static int[] parent;
-    static ArrayList<Edge> adjGraph = new ArrayList<>();
+    static ArrayList<Edge> adj = new ArrayList<>();
 
-    static class Edge implements Comparable<Edge>{
-        int nodeA;
-        int nodeB;
-        int weight;
+    static class Edge implements Comparable<Edge> {
 
-        Edge(int nodeA, int nodeB, int weight) {
-            this.nodeA = nodeA;
-            this.nodeB = nodeB;
-            this.weight = weight;
+        int a;
+        int b;
+        int w;
+
+        Edge(int a, int b, int w) {
+            this.a = a;
+            this.b = b;
+            this.w = w;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(this.weight, o.weight);
+        public int compareTo(Main.Edge o) {
+            return Integer.compare(this.w, o.w);
         }
+
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        parent = new int[N+1];
-        for(int i=0; i<=N; i++) {
+        parent = new int[V+1];
+        for(int i=1; i<=V; i++) {
             parent[i] = i;
         }
 
-
-        for(int i=1; i<=M; i++) {
+        for(int i=0; i<E; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            adjGraph.add(new Edge(a, b, w));
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+
+            adj.add(new Edge(A, B, W));
         }
+        Collections.sort(adj);
 
-        // 가중치 순 오름차순 정렬, 이렇게 해도 전체 배열이 정렬되나?
-        Collections.sort(adjGraph);
-
-        //추가로, ArrayList<Edge>[] 의 배열을 weight순으로 전체 정렬 한다면?
-
-        // 인접리스트를 배열로 만들어서 한 노드를 배열인덱스로 담고, 나머지 노드를 원소로 추가해서 쓰는 이유,
-        // 그냥 배열 쓰지 말고 두 노드 다 원소로 쓰면 안되나? 
-
-        int cnt = 0;
-        long totalWeight=0;
-        for(Edge edge:adjGraph) {
-            int nodeA = edge.nodeA;
-            int nodeB = edge.nodeB;
-            if(find(nodeA) != find(nodeB)) {
-                union(nodeA, nodeB);
-                totalWeight += edge.weight;
+        long cnt = 0;
+        long sum = 0;
+        for(int i=0; i<E; i++){
+            Edge now = adj.get(i);
+            int A = now.a;
+            int B = now.b;
+            int W = now.w;
+            if(union(A,B)) {
+                cnt++;
+                sum += W;
             }
-            if(cnt == N-1) break;
+
+            if(cnt==V-1) {
+                break;
+            }
         }
-        System.out.println(totalWeight);
-    }
+        System.out.println(sum);
+        
+    }   
 
-    static int find(int a) {
-        if(parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
-    }
-
-    static void union(int a, int b) {
+    static boolean union(int a, int b) {
         int ra = find(a);
         int rb = find(b);
 
-        if(ra>rb)
-            parent[ra] = rb;
-        else 
-            parent[rb] = ra;
+        if(ra==rb) return false;
+
+        // ra랑 rb로 parent에 대입하는거 맞지?
+        if(ra>rb) parent[ra] = rb;
+        else parent[rb] = ra;
+
+        return true;
+    }   
+
+    static int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
     }
+     
 }
