5a798d59af49a0ff97b8e91bb7ca2ddb1b005656
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-21 12:43:17 +0900

    [Silver II] Title: 이항 계수 2, Time: 192 ms, Memory: 21636 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -7,29 +7,28 @@
 
 
     public class Main {
+        static final int MOD = 10007;
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int N = sc.nextInt();
-            int K = sc.nextInt();
-            if(N==K || K==0) {
-                System.out.println(1);
-                return;
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            if(n==k || k==0) {
+                System.out.println(1%MOD); return;
             }
 
-            int[][] dp = new int[N+1][N+1];
-
-            // nc0 = 1, ncn = 1
-            for(int i=0; i<=N; i++){
-                dp[i][0] = 1;
-                dp[i][i] = 1;
+            int[][] d = new int[n+1][n+1];
+            for(int i=0; i<=n; i++){
+                d[i][0] = 1;
+                d[i][i] = 1;
             }
 
-            for(int i=2; i<=N; i++){
+            for(int i=2; i<=n; i++){
                 for(int j=1; j<i; j++){
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
-                    dp[i][j] %= 10007;
+                    d[i][j] = d[i-1][j] + d[i-1][j-1];
+                    d[i][j] %= MOD;
                 }
             }
-            System.out.println(dp[N][K]);
+            System.out.println(d[n][k]);
         }
     }
