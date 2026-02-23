e46f8300935ff39068dcdb9e206d02b666593007
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-23 12:41:04 +0900

    [Bronze II] Title: 피보나치 수, Time: 168 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -6,30 +6,19 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] D;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        D = new int[N+1];
-        for(int i=0; i<=N; i++) {
-            D[i] = -1;
+        int N = sc.nextInt();        
+        int[] dp = new int[46];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<46; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        D[0] = 0;
-        D[1] = 1;
 
-        // 바텀업 방식
-        for(int i=2; i<=N; i++) {
-            D[i] = D[i-1] + D[i-2];
-        }
-        // 탑다운 방식
-        // fibo(N);
+        System.out.println(dp[N]);
 
-
-        System.out.println(D[N]);
-    }
-
-    static int fibo(int N){
-        if(D[N] != -1) return D[N]; // DP의 장점, 기존에 있는 해답은 추가적인 연산 없이 반환 가능
-        return D[N] = fibo(N-2) + fibo(N-1); 
     }
 }
