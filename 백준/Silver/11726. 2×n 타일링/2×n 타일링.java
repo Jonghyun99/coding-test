d1c48e534abdfbee0fc7e92d7a7f7e43fe17729f
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-02-24 14:10:29 +0900

    [Silver III] Title: 2×n 타일링, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] D = new int[1001];
+        D[1] = 1;
+        D[2] = 2;
+
+        for(int i=3; i<=N; i++) {
+            D[i] = (D[i-1] + D[i-2]) % 10007;
+        }
+        System.out.println(D[N]);
+
+    }
+}
