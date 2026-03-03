636d9c4abf17c077735e353219177a99e649cdb6
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-03-03 12:10:31 +0900

    [level 2] Title: 문자열 압축, Time: 3.64 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 88.1 MB, 시간: 4.38 ms
+메모리: 81.7 MB, 시간: 3.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 01일 09:56:07
+2026년 03월 03일 12:10:30
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,34 +1,37 @@
 class Solution {
     public int solution(String s) {
+        if(s.length()==1) return 1;
+        
         int minLength = s.length();
         
-        for(int i=1; i<=s.length() / 2 ;i++) {
+        for(int i=1; i<=s.length()/2; i++){
+            String base = s.substring(0,i);
+            int count = 1;
             StringBuilder sb = new StringBuilder();
-            String prev = s.substring(0,i);
-            int count=1;
-            
             for(int j=i; j<s.length(); j+=i) {
-                int end = Math.min(j+i, s.length()); // 포인트 indexOutof 방지
-                String curr = s.substring(j,end);
+                int lastIndex = Math.min(s.length(), i+j);
+                String curr = s.substring(j,lastIndex);
                 
-                if(prev.equals(curr)) {
+                if(base.equals(curr)) {
                     count++;
                 } else {
-                    if(count>=2) {
+                    if(count>1) {
                         sb.append(count);
                     }
-                    sb.append(prev);
-                    
-                    prev = curr;
+                    sb.append(base);
+                    base=curr;
                     count=1;
                 }
             }
-            if(count>=2) {
+            if(count>1) {
                 sb.append(count);
             }
-            sb.append(prev);
-            minLength = Math.min(minLength, sb.toString().length());
+            sb.append(base);
+            
+            minLength = Math.min(sb.length(),minLength);
         }
+        
+        
         return minLength;
     }
 }
