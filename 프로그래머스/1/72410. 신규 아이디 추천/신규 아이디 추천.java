b3866b629b1c2f62b1790df32816c8e771d28d10
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-03-03 11:07:01 +0900

    [level 1] Title: 신규 아이디 추천, Time: 22.24 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -4,18 +4,16 @@ class Solution {
             .replaceAll("[^a-z0-9-_.]","")
             .replaceAll("\\.{2,}",".")
             .replaceAll("^[.]|[.]$","");
-        // if(answer.startsWith(".") && answer.length()>2) answer = answer.substring(1,answer.length()-1);
-        // if(answer.endsWith(".")) answer = answer.substring(0,answer.length()-2);
-        
         if(answer.isEmpty()) answer = "a";
         
-        if(answer.length() >= 16) {
+        if(answer.length()>=16) {
             answer = answer.substring(0,15);
-            if(answer.endsWith(".")) answer = answer.substring(0,14);
+            if(answer.endsWith("."))
+                answer = answer.substring(0,14);
         }
         
         while(answer.length()<3) {
-            answer+=answer.charAt(answer.length()-1);
+            answer += answer.charAt(answer.length()-1);
         }
         return answer;
     }
