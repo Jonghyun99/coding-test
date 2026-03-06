7d0c8e3b2a57dfa816a029aa7dd3e47ad97f2ec3
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-03-06 18:35:41 +0900

    [level 1] Title: 폰켓몬, Time: 6.44 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,14 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
         
-        // 가장 많은 종류의 포켓몬 구하기
-        for(int num:nums) {
-            map.put(num,1);
+        Set<Integer> set = new HashSet<>();
+        for(int n:nums) {
+            set.add(n);
         }
         
-        
-        return Math.min(map.size(), nums.length/2);
+        return Math.min(set.size(),nums.length/2);
     }
 }
