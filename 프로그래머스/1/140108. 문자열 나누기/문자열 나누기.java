94e26aa5bc67f4c1e2700100c67aad38136b70e2
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-03-07 09:44:03 +0900

    [level 1] Title: 문자열 나누기, Time: 0.86 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,33 +1,24 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        
         char x = ' ';
         int xCount = 0;
         int otherCount = 0;
-        
-        for(int i=0; i<s.length(); i++) {
-            char target = s.charAt(i);
-            if(xCount == 0 && otherCount == 0) {
-                x = target;
+        int answer = 0;
+        for(char c:s.toCharArray()) {
+            if(xCount==0 && otherCount == 0) {
+                x = c;
             }
             
-            if(x==target) {
-                xCount++;
-            } else {
-                otherCount++;
-            }
+            if(x==c) xCount++;
+            else otherCount++;
             
-            if(xCount == otherCount) {
+            if(xCount==otherCount){
                 answer++;
-                xCount = 0;
-                otherCount = 0;
+                xCount=0;
+                otherCount=0;
             }
         }
-        if(xCount!=0 || otherCount !=0){
-            answer++;
-        }        
-        
+        if(xCount!=0 || otherCount != 0) answer++;
         return answer;
     }
 }
