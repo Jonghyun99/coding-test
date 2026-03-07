8c7bdc1469c0602453caa188b2fe565ba69e8ebf
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-03-07 09:14:33 +0900

    [level 2] Title: 문자열 압축, Time: 2.81 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,37 +1,32 @@
 class Solution {
     public int solution(String s) {
-        if(s.length()==1) return 1;
-        
         int minLength = s.length();
         
-        for(int i=1; i<=s.length()/2; i++){
-            String base = s.substring(0,i);
-            int count = 1;
+        for(int i=1; i<=s.length()/2; i++) {
             StringBuilder sb = new StringBuilder();
+            String curr =  s.substring(0,i);
+            int count = 1;
             for(int j=i; j<s.length(); j+=i) {
-                int lastIndex = Math.min(s.length(), i+j);
-                String curr = s.substring(j,lastIndex);
-                
-                if(base.equals(curr)) {
+                int lastIndex = Math.min(s.length(), j+i);
+                String next = s.substring(j,lastIndex);
+                if(curr.equals(next)) {
                     count++;
                 } else {
-                    if(count>1) {
+                    if(count>=2){
                         sb.append(count);
+                        count = 1;
                     }
-                    sb.append(base);
-                    base=curr;
-                    count=1;
+                    sb.append(curr);
                 }
+                curr = next;
             }
-            if(count>1) {
+            if(count>=2) {
                 sb.append(count);
             }
-            sb.append(base);
+            sb.append(curr);
             
-            minLength = Math.min(sb.length(),minLength);
+            minLength = Math.min(minLength, sb.length());
         }
-        
-        
         return minLength;
     }
 }
