eef18bec3b7345d316307f9443a4efb9e5afb71c
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-03-08 08:56:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.95 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,53 @@
 import java.util.*;
+
 class Solution {
-    
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static boolean[][] visited;
-    
+    static int[] drow = {1,0,-1,0};
+    static int[] dcol = {0,1,0,-1};
     public int solution(int[][] maps) {
-        visited = new boolean[maps.length+1][maps[0].length+1];
-        bfs(0,0, maps);
-        int cnt = maps[maps.length-1][maps[0].length-1];
-        if(cnt==1) return -1;
-        return cnt-1;
+        
+        int maxRow = maps.length-1;
+        int maxCol = maps[0].length-1;
+        
+        bfs(0,0,maps, maxRow, maxCol);
+        if(maps[maxRow][maxCol] == 1) return -1;
+        
+        return maps[maxRow][maxCol] - 1;
     }
     
-    static void bfs(int N, int M, int[][] maps) {
+    static void bfs(int startRow, int startCol, int[][] maps, int maxRow, int maxCol) {
         Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[] {N,M});
-        maps[N][M] = maps[N][M] + 1;
+        q.add(new int[] {startRow, startCol});
+        
+        
+        
+        maps[startRow][startCol]++;
+        
+        
+        
         while(!q.isEmpty()) {
             int[] now = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
+            int nowRow = now[0];
+            int nowCol = now[1];
             
-            if(nowX==maps.length-1 && nowY==maps[nowX].length -1) {
-                return;
-            }
+            if(nowRow == maxRow && nowCol == maxCol) return;
             
             for(int i=0; i<4; i++) {
-                int nextX = nowX+dx[i];
-                int nextY = nowY+dy[i];
-                if(nextX<maps.length && nextY<maps[0].length && nextX>=0 && nextY>=0){
-                    if(maps[nextX][nextY] == 1) {
-                        maps[nextX][nextY] = maps[nowX][nowY] + 1;
-                        q.add(new int[] {nextX, nextY});
-                    }
+                int nextRow = nowRow + drow[i];
+                int nextCol = nowCol + dcol[i];
+                
+                if(nextRow < 0 || nextCol < 0 || nextRow > maxRow || nextCol > maxCol) 
+                   continue; 
+                
+                if(maps[nextRow][nextCol] == 1) {
+                    maps[nextRow][nextCol] = maps[nowRow][nowCol] + 1;
+                    q.add(new int[] {nextRow, nextCol});
                 }
+                
             }
         }
     }
 }
+
+
+
+
