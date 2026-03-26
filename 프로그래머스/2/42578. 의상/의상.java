96beeaa95c966f4e4a0ab7585339d55f6773ec0b
Jonghyun <75297568+Jonghyun99@users.noreply.github.com>
2026-03-26 18:13:24 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,22 +1,15 @@
 import java.util.*;
 class Solution {
-    
-    // 1. 종류 마다 1개씩만 착용 가능
-    // 2. 조합하여 나올 수 있는 경우의 수
-    // -> 타입 별 개수를 저장한 뒤 조합
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> m = new HashMap<>();
         for(String[] c:clothes) {
-            String name = c[0];
-            String type = c[1];
-            
-            map.put(type,map.getOrDefault(type,0)+1);
+            m.put(c[1], m.getOrDefault(c[1],0)+1);
         }
         
-        int maxCombinations = 1;
-        for(int val:map.values()) {
-            maxCombinations *= (val +1); // 해당 옷을 안입는 경우 포함
+        int combo = 1;
+        for(int v:m.values()) {
+            combo *= v+1;
         }
-        return maxCombinations-1;
+        return combo-1;
     }
 }
